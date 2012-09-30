bd1d1202a54c7315cb7ed82cf496b64c2be7330f
Elizabet Hunt <ezoehunt@gmail.com>
2012-09-30 13:15:42 -0700

    about

--- wp-content/themes/mytheme/page-about.php
+++ wp-content/themes/mytheme/page-about.php
@@ -12,9 +12,18 @@
 			<div id="content" class="about">
 				<h3 class="page-title">CityHow — making it easy to find and share information about working for the City of Philadelphia.</h3>
 
-<p>Brought to you by <a class="nhline" title="Go to Code for America" href="http://www.codeforamerica.org" target="_blank">Code for America</a> and the <a class="nhline" title="Go to City of Philadelphia" href="http://www.phila.gov" target="_blank">City of Philadelphia</a>, CityHow is a place to collect and share knowledge about work topics like booking a conference room in City Hall or signing up for a City Zipcar account.</p>
+<!--p>Brought to you by <a class="nhline" title="Go to Code for America" href="http://www.codeforamerica.org" target="_blank">Code for America</a> and the <a class="nhline" title="Go to City of Philadelphia" href="http://www.phila.gov" target="_blank">City of Philadelphia</a>, CityHow is a place to collect and share knowledge about work topics like booking a conference room in City Hall or signing up for a City Zipcar account.</p>
 
-<p>A CityHow Guide can be about anything you think would be useful to other people working for the City of Philadelphia. Short or long, <strong>if it's something you know how to do, it's probably something other people want to know how to do. So share your CityHow.</strong></p>
+<p>A CityHow Guide can be about anything you think would be useful to other people working for the City of Philadelphia. Short or long, <strong>if it's something you know how to do, it's probably something other people want to know how to do. So share your CityHow.</strong></p-->
+	
+<?php 
+if (have_posts()) :
+while (have_posts()) : 
+the_post();
+the_content();
+endwhile;
+endif;
+?>
 
 <p style="text-align: center;"><a style="padding: 0 2em 0 1em;" title="Go to Code for America" href="http://www.codeforamerica.org" target="_blank"><img src="<?php echo $style_url;?>/images/logo_cfa_color.png" alt="Code for America logo" /></a> <a style="padding: 0 1em 0 2em;" title="Go to City of Philadelphia" href="http://www.phila.gov" target="_blank"><img src="<?php echo $style_url;?>/images/logo_phl_color.png" alt="City of Philadelphia logo" /></a></p>
 		
