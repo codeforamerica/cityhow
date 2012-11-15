a110fd69c0415c6fc4b770b52847776357860957
Elizabet Hunt <ezoehunt@gmail.com>
2012-11-14 21:16:58 -0800

    update bkup

--- scripts/mysql_dump.sh
+++ scripts/mysql_dump.sh
@@ -59,7 +59,7 @@ else
 	MAILCONTENT="quiet"
 	
 	# Set the maximum allowed email size in k. (4000 = approx 5MB email [see docs])
-	MAXATTSIZE="4000"
+	MAXATTSIZE="4050"
 	
 	# Email Address to send mail to? (user@domain.com)
 	MAILADDR="information@cityhow.org"
