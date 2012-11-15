baa1c67e039d7d7d520b14e87b3802cfcef1dea3
Elizabet Hunt <ezoehunt@gmail.com>
2012-11-14 20:02:08 -0800

    remove edit flow plugin

--- wp-content/plugins/edit-flow/common/js/ef_date.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery('.date-pick')
-		.datepicker({
-			dateFormat: 'M dd yy',
-			firstDay: ef_week_first_day,
-		});
-});
--- wp-content/plugins/edit-flow/modules/notifications/lib/notifications.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery('#ef-post_following_users_box ul').listFilterizer();	
-});
