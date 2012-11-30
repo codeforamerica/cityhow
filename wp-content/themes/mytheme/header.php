61749cd87f44d72e0024fcc4ee5d94f5fee0d6a5
Elizabet Hunt <ezoehunt@gmail.com>
2012-11-29 22:57:41 -0800

    removed kissmetrics code

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -59,29 +59,7 @@ $links = 'current-item';
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
-<?php
-// Kissmetrics - remove before handover to Philly
-?>
-<script type="text/javascript">
-  var _kmq = _kmq || [];
-  var _kmk = _kmk || '007f169f0f432e69426fc8c264cfc753adc023c6';
-  function _kms(u){
-    setTimeout(function(){
-      var d = document, f = d.getElementsByTagName('script')[0],
-      s = d.createElement('script');
-      s.type = 'text/javascript'; s.async = true; s.src = u;
-      f.parentNode.insertBefore(s, f);
-    }, 1);
-  }
-  _kms('//i.kissmetrics.com/i.js');
-  _kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
-</script>
-<script type="text/javascript">
-  _kmq.push(['identify', '<?php echo json_encode($current_user->user_login); ?>']);
-</script>
-<?php
-// End Kissmetrics
-?>
+
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width; initial-scale=1.0">
 
